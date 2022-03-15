b4f0354c73f4d57ca08b81cc0c5ccdce7a79840c
Jessie <iamseye@gmail.com>
2022-03-14 20:54:56 -0400

    update directry page with directory & category item component

---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,18 @@
+import "./category-item.styles.scss";
+
+const CategoryItem = ({ category: { id, title, imageUrl } }) => (
+  <div className="category-container" key={id}>
+    <div
+      className="background-image"
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}
+    />
+    <div className="category-body-container">
+      <h2>{title}</h2>
+      <p>Showp Now</p>
+    </div>
+  </div>
+);
+
+export default CategoryItem;
