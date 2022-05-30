b3c231f6ddf41111d7c5c768478c5e63cc7e9931
jessiehuang <iamseye@gmail.com>
2022-05-30 17:46:48 -0400

    change to custom logger and add reselect to memorise the categories map

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -13,8 +13,6 @@ const Shop = () => {
     useEffect(() => {
         const getCategoriesMap = async () => {
            const categoriesArray = await getCategoriesAndDocuments();
-           console.log('123')
-           console.log(categoriesArray)
            dispatch(setCategories(categoriesArray));
         };
         getCategoriesMap();
--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,9 +1,21 @@
-export const selectCategoriesMap = (state) => {
-    return state.categories.categories.reduce(
-        (acc, {title, items}) => {
-            acc[title.toLowerCase()] = items;
-            return acc;
-        },
-        {}
-    );
-};
+import {createSelector} from "reselect";
+
+const selectCategoryReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+    [selectCategoryReducer],
+    (categoriesSlice) => categoriesSlice.categories
+);
+
+export const selectCategoriesMap = createSelector(
+    [selectCategories],
+    (categories) =>
+    {
+        return categories.reduce(
+            (acc, {title, items}) => {
+                acc[title.toLowerCase()] = items;
+                return acc;
+            },
+            {}
+        );
+    });
