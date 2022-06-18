cc60babcb9227468f659e1306985ffb00ccd3f14
iamseye <iamseye@gmail.com>
2022-06-18 15:38:34 -0400

    chore: try connect with stripe

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -13,11 +13,43 @@ const PaymentForm = () => {
         if (!stripe || !elements) {
             return;
         }
+
+        const response = await fetch(
+            '/.netlify/functions/create-payment-intent',
+            {
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ amount: 10000 }),
+            }
+        ).then((res) => res.json());
+
+        const {
+            paymentIntent: { client_secret },
+        } = response;
+
+        const paymentResult = await stripe.confirmCardPayment(client_secret, {
+            payment_method: {
+                card: elements.getElement(CardElement),
+                billing_details: {
+                    name: 'user_name_here',
+                },
+            },
+        });
+
+        if (paymentResult.error) {
+            alert(paymentResult.error?.message);
+        } else {
+            if (paymentResult.paymentIntent.status === 'succeeded') {
+                alert('payment succeeded');
+            }
+        }
     };
 
     return (
         <PaymentFormContainer>
-            <FormContainer>
+            <FormContainer onSubmit={paymentHandler}>
                 <h2>Credit Card Payment: </h2>
                 <CardElement />
                 <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>
